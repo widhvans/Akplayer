45355386ccb48c4661b6c34c61a8b125ae43256f
Athula1996 <as772685@gmail.com>
2025-12-16 17:03:44 +0530

    Fix duplicate formatTime function build error

--- app/src/main/java/com/provideoplayer/PlayerActivity.kt
+++ app/src/main/java/com/provideoplayer/PlayerActivity.kt
@@ -1600,23 +1600,9 @@ class PlayerActivity : AppCompatActivity() {
                 retriever.release()
             } catch (e: Exception) {
                 // Ignore thumbnail errors
-            }
         }
     }
     
-    private fun formatTime(ms: Long): String {
-        val totalSeconds = ms / 1000
-        val hours = totalSeconds / 3600
-        val minutes = (totalSeconds % 3600) / 60
-        val seconds = totalSeconds % 60
-        
-        return if (hours > 0) {
-            String.format("%d:%02d:%02d", hours, minutes, seconds)
-        } else {
-            String.format("%02d:%02d", minutes, seconds)
-        }
-    }
-
     private fun showGestureIndicator(type: GestureType, value: Int) {
         binding.gestureIndicator.visibility = View.VISIBLE
         when (type) {
